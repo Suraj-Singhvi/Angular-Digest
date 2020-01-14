af1419e91b389f9451e3dfd8c7334770c11ed71b
Suraj Singhvi <surajsinghvi11@gmail.com>
2020-01-14 12:50:19 +0530

    added header component

--- src/app/header/header.module.ts
+++ src/app/header/header.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header.component';
-
+import { MatMenuModule, MatButtonModule } from '@angular/material';
+import { RouterModule } from '@angular/router';
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule, MatMenuModule, RouterModule],
   exports: [HeaderComponent]
 })
 export class HeaderModule {}
