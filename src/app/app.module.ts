f581e3c6d0d1d9c1b5f36af081ba4733c0fc817f
Suraj Singhvi <surajsinghvi11@gmail.com>
2020-01-14 13:13:37 +0530

    remove side-nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderModule } from './header/header.module';
-import { SideNavModule } from './side-nav/side-nav.module';
 import { FooterModule } from './footer/footer.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, HeaderModule, SideNavModule, FooterModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, HeaderModule, FooterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
