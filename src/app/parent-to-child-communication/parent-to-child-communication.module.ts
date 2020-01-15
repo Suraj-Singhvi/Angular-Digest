d44ff26c2b2f1e4d93ed6010fcd159be72de2a7d
Suraj Singhvi <surajsinghvi11@gmail.com>
2020-01-15 20:06:51 +0530

    add parent-to-child communication example

--- src/app/parent-to-child-communication/parent-to-child-communication.module.ts
+++ src/app/parent-to-child-communication/parent-to-child-communication.module.ts
@@ -5,9 +5,12 @@ import { ParentComponent } from './parent/parent.component';
 import { ChildComponent } from './child/child.component';
 import { ParentToChildCommunicationComponent } from './parent-to-child-communication.component';
 import { ParentToChildCommunicationRoutingModule } from './parent-to-child-communication-routing.module';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ParentComponent, ChildComponent, ParentToChildCommunicationComponent],
-  imports: [CommonModule, ParentToChildCommunicationRoutingModule]
+  imports: [CommonModule, ParentToChildCommunicationRoutingModule, MatCardModule, MatInputModule, FormsModule]
 })
 export class ParentToChildCommunicationModule {}
--- src/app/parent-to-child-communication/parent/parent.component.ts
+++ src/app/parent-to-child-communication/parent/parent.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./parent.component.scss']
 })
 export class ParentComponent implements OnInit {
+  parentMessage = 'This message is from Parent';
   constructor() {}
 
   ngOnInit() {}
