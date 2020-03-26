1e886e8fbe6e843ab0fd3de717af8067f8f0a5b9
Suraj Singhvi <surajsinghvi11@gmail.com>
2020-03-27 00:54:19 +0530

    update parent to child communicaton example

--- src/app/parent-to-child-communication/child/child.component.ts
+++ src/app/parent-to-child-communication/child/child.component.ts
@@ -2,12 +2,30 @@ import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-child',
-  templateUrl: './child.component.html',
-  styleUrls: ['./child.component.scss']
+  template: `
+    <mat-card class="child-card">
+      <mat-card-title>Child Component</mat-card-title>
+      here is some message from parent:
+
+      <p>{{ childObj.sender }} AKA {{ aliasName }}, Age {{ age }}:- quoted {{ childObj.message }}</p>
+      <form class="example-form">
+        <mat-form-field class="example-full-width">
+          <input matInput placeholder="Favorite food" [value]="childMessage" />
+        </mat-form-field>
+      </form>
+    </mat-card>
+  `
 })
 export class ChildComponent implements OnInit {
   @Input() childMessage;
+  @Input() childObj;
+  // tslint:disable-next-line: no-input-rename
+  @Input('nickName') aliasName;
+  @Input() age;
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    console.log('age is of type: ', typeof this.age);
+  }
 }
--- src/app/parent-to-child-communication/parent/parent.component.ts
+++ src/app/parent-to-child-communication/parent/parent.component.ts
@@ -2,11 +2,37 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-parent',
-  templateUrl: './parent.component.html',
+  template: `
+    <mat-card class="parent-card">
+      <form class="example-form">
+        <mat-form-field class="example-full-width">
+          <input
+            matInput
+            placeholder="Favorite food"
+            [value]="parentMessage"
+            [(ngModel)]="parentMessage"
+            name="something"
+          />
+        </mat-form-field>
+      </form>
+
+      <app-child
+        class="child-message"
+        [childMessage]="parentMessage"
+        [childObj]="parentObj"
+        [nickName]="'GodFather'"
+        [age]="64"
+      ></app-child>
+    </mat-card>
+  `,
   styleUrls: ['./parent.component.scss']
 })
 export class ParentComponent implements OnInit {
   parentMessage = 'This message is from Parent';
+  parentObj = {
+    sender: 'Howard Stark',
+    message: 'You know nothing.'
+  };
   constructor() {}
 
   ngOnInit() {}
