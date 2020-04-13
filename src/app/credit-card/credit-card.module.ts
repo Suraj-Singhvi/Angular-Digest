2fab4ed15b7e99d866295ad623396db1b5d4fcee
Suraj Singhvi <surajsinghvi11@gmail.com>
2020-04-13 16:40:18 +0530

    created a tooltip directive

--- src/app/credit-card/credit-card.module.ts
+++ src/app/credit-card/credit-card.module.ts
@@ -4,6 +4,7 @@ import { CreditCardComponent } from './credit-card.component';
 import { CreditCardDirective } from './credit-card-directive/credit-card.directive';
 import { Routes, RouterModule } from '@angular/router';
 import { MatCardModule, MatInputModule } from '@angular/material';
+import { TooltipDirective } from './tooltip-directive/tooltip.directive';
 
 const routes: Routes = [
   {
@@ -13,7 +14,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  declarations: [CreditCardComponent, CreditCardDirective],
+  declarations: [CreditCardComponent, CreditCardDirective, TooltipDirective],
   imports: [CommonModule, RouterModule.forChild(routes), MatCardModule, MatInputModule]
 })
 export class CreditCardModule {}
